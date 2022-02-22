ab254ad2c4ae46d11699384135adc68f08081933
Benji Stephenson <bjames.stephenson@gmail.com>
2022-02-23 08:54:53 +1300

    Add record validation. NEL functions return copies of the original array

--- src/NonEmptyArray/index.ts
+++ src/NonEmptyArray/index.ts
@@ -4,8 +4,9 @@ export type NonEmptyArray<T> = Array<T> & {
 
 export const isNonEmptyArray = <T>(list: Array<T>): list is NonEmptyArray<T> => list.length > 0
 
-export const head: <T>(l: NonEmptyArray<T>) => T = (l) => l[0]
+export const head: <T>(l: NonEmptyArray<T>) => T = (l) => l.slice(0, 1)[0]
 export const tail: <T>(l: NonEmptyArray<T>) => T[] = (l) => l.slice(1)
 
 export const concat = <A>(x: NonEmptyArray<A>, y: NonEmptyArray<A>): NonEmptyArray<A> =>
-  [...x, ...y] as NonEmptyArray<A>
+  x.slice().concat(y.slice()) as NonEmptyArray<A>
+//  [...x, ...y] as NonEmptyArray<A>
--- src/Record/index.ts
+++ src/Record/index.ts
@@ -4,6 +4,35 @@ export const keys = <K extends string>(r: Record<K, any>): K[] => Object.keys(r)
 
 export const unit: Record<string, never> = {}
 
+// export function sequenceR<R extends Record<string, E.Either<string, any>>>(
+//   record: R
+// ): E.Either<string[], { [K in keyof R]: R[K] extends E.Either<string[], infer A> ? A : never }>
+// export function sequenceR(record: Record<string, E.Either<string, any>>): E.Either<string[], Record<string, any>> {
+//   const [head, ...tail] = Object.keys(record)
+
+//   const initial = record[head].map(val => ({ [head]: val })).mapLeft(s => [s])
+
+//   return tail.reduce(
+//     (acc, key) => {
+//       return E.match({
+//         Left: err =>
+//           E.match({
+//             Left: errs => E.left([...errs, ...err]),
+//             Right: _ => E.left([err]),
+//           }, acc),
+//         Right: val =>
+//           E.match({
+//             Left: errs => E.left(errs),
+//             Right: rec => E.right({ ...rec, [key]: val }),
+//           }, acc)
+//       },
+//         record[key]
+//       )
+//     },
+//     initial
+//   )
+// }
+
 export const traverse =
   <F extends HKT>(F: Applicative<F>) =>
   <R, E, A, B>(f: (a: A) => Kind<F, R, E, B>, ra: Record<string, A>): Kind<F, R, E, Record<string, B>> => {
@@ -27,7 +56,15 @@ export const traverse =
     }, initial)
   }
 
-export const sequence =
-  <F extends HKT>(F: Applicative<F>) =>
-  <R, E, A>(fa: Record<string, Kind<F, R, E, A>>): Kind<F, R, E, Record<string, A>> =>
-    traverse(F)((x) => x, fa)
+// export function sequenceR<R extends Record<string, E.Either<string, any>>>(
+//   record: R
+// ): E.Either<string[], { [K in keyof R]: R[K] extends E.Either<string[], infer A> ? A : never }>
+
+export function sequence<F extends HKT>(
+  F: Applicative<F>
+): <R, E, Rec extends Record<string, Kind<F, R, E, any>>>(
+  fa: Rec
+) => Kind<F, R, E, { [K in keyof Rec]: Rec[K] extends Kind<F, R, E, infer A> ? A : never }>
+export function sequence<F extends HKT>(F: Applicative<F>) {
+  return <R, E>(fa: Record<string, Kind<F, R, E, any>>): Kind<F, R, E, Record<string, any>> => traverse(F)((x) => x, fa)
+}
